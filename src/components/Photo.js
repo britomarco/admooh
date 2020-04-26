057cc05dc1b9f3e05bcd039b5e247c92ba002994
Marco <marco.brito@siliconlife.ai>
2020-04-26 17:43:03 -0300

    add modal

--- src/components/Photo.js
+++ src/components/Photo.js
@@ -1,12 +1,47 @@
 import React from 'react'
 import Grid from '@material-ui/core/Grid'
+import Modal from '@material-ui/core/Modal';
+import './photo.scss'
 
-export const Photo = ({photo}) => (
-    <>
-        <Grid container item xs={12} sm={3} className='item-album' spacing={1}>
-                <h2>
-                    {photo.title}
-                </h2>
-        </Grid>
-    </>
-)
+export const Photo = ({photo}) => {
+    const [open, setOpen] = React.useState(false);
+
+    const handleOpen = () => {
+      setOpen(true);
+    };
+  
+    const handleClose = () => {
+      setOpen(false);
+    };
+  
+
+    return(
+        <>
+            <Grid 
+                container 
+                item 
+                xs={12} 
+                sm={3} 
+                justify="center" 
+                className='item-photo'
+                onClick={handleOpen}
+            >
+                <img src={photo.thumbnailUrl} alt={photo.title}/>
+            </Grid>
+            
+            <Modal
+                className="modal-body"
+                open={open}
+                onClose={handleClose}
+                alignItems="center"
+                justify="center"
+            >
+                <div>
+                   <img src={photo.url} alt={photo.title}/>
+                </div>
+            </Modal>
+        </>
+    )
+}
+    
+
